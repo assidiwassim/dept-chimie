f20bf0ce0215303c675f4f96ce787d5c153adb73
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-01-22 17:28:19 +0100

    wassim

--- config/database.php
+++ config/database.php
@@ -45,7 +45,7 @@ return [
             'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'dept-chimie-db'),
-            'username' => env('DB_USERNAME', 'Moha20med1994'),
+            'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
