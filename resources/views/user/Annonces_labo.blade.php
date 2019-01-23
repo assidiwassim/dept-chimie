456f6c9970ea074f966aa4b02ef0790f2b7f7e86
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-01-24 00:49:44 +0100

    wassim

--- resources/views/user/Annonces_labo.blade.php
+++ resources/views/user/Annonces_labo.blade.php
@@ -58,18 +58,46 @@
     background-color: #ff396c;
     border: 1px solid white;
 }
+
+.box-header-cover .demande{
+    color: #fff;
+    font-size: 14px;
+    font-weight: 700;
+    border-radius: 50px;
+    padding: 2px 15px;
+    position: absolute;
+    top: 10px;
+    left:10px;
+    background-color: #4CAF50;
+    border: 1px solid white;
+}
+
+
 .box-body h2{
     font-size: 18px;
     font-weight: 700;
     color: #000;
 }
+.box-body p {
+    height: 60px;
+    overflow: auto;
+}
 .box-footer .btn-repondre{
+    background-color: #fff;
+}
+.box-footer .btn-repondre-Demande{
     background-color: #fff;
 }
 .box-footer .btn-repondre:hover{
     background-color: #ff396c;
     color:white;
-    border:none;
+    border-color:transparent;
+}
+
+.box-footer .btn-repondre-Demande:hover{
+    background-color: #4CAF50;
+    color:white;
+    border-color:transparent;
 }
 
 #search-form .input-container{
@@ -137,7 +165,7 @@
                     </div>
                     <div class="col-md-2">
                             <select name="reference" class="form-control select1">
-                                    <option selected disabled>Référence</option>
+                                    <option selected disabled>Catégorie</option>
                                     <option>option 2</option>
                                     <option>option 3</option>
                                     <option>option 4</option>
@@ -146,7 +174,7 @@
                      </div>
                     <div class="col-md-2">
                             <select name="formule" class="form-control select2">
-                                    <option selected disabled>Formule</option>
+                                    <option selected disabled>Type d'annonce</option>
                                     <option>option 2</option>
                                     <option>option 3</option>
                                     <option>option 4</option>
@@ -164,7 +192,8 @@
     </div>
 
     <div class="row row_list_produit">
-            <div class="col-md-3">
+          
+              <div class="col-md-3 col-sm-4  ">
               <div class="box box-animation" style="padding:0 !important;">
                <div class="box-header boxImg" style="background-image:url('https://img.e-marketing.fr/Img/BREVE/2017/2/313628/Comment-optimiser-son-experience-utilisateur-mobile-F.jpg');">
                 <div class="box-header-cover">
@@ -183,16 +212,25 @@
                 </div>
               </div>
             </div>
-            <div class="col-md-3">
-                    <div class="box">
-                      <div class="box-body">
-                          <div class="img-container">
-                                  <img src="{{ asset("img/produit.jpg")}}" class="img-responsive">
-                          </div>
-                     
-                      </div>
-                    </div>
-                  </div>
+              <div class="col-md-3 col-sm-4 ">
+              <div class="box box-animation" style="padding:0 !important;">
+               <div class="box-header boxImg" style="background-image:url('https://img.e-marketing.fr/Img/BREVE/2017/2/313628/Comment-optimiser-son-experience-utilisateur-mobile-F.jpg');">
+                <div class="box-header-cover">
+                   <a class="btn btn-default btn-success demande ">Demande</a>
+                    <h1>Nom de labo</h1>
+                </div>
+               </div>
+                <div class="box-body" >
+                  <h2>Titre de l'annonce</h2>
+                  <p>
+                        Lorem ipsum dolor sit amet, ut graece omnesque assentior sit, quo eu tota scripserit cotidieque
+                  </p>
+                </div>
+                <div class="box-footer">
+                    <a class="btn btn-default btn-lg btn-block btn-repondre-Demande ">Répondre</a>
+                </div>
+              </div>
+            </div>
           </div>
    
 </section>
