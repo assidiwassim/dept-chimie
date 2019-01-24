9b57e4a7fb6b8da2e592785dac3f4b168857ad2b
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-01-24 16:30:01 +0100

    Update ajouter_annonce.blade.php

--- resources/views/user/ajouter_annonce.blade.php
+++ resources/views/user/ajouter_annonce.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.layout-labo')
 
 @section('content')
+
 <style>
 .info-box {
     box-shadow: 0px 3px 13px 3px #00000014;
@@ -55,16 +56,28 @@
     padding: 15px;
 }
 
+fieldset
+{
+  background-color:#CCC;
+
+  padding:16px;	
+}
+.legend1
+{
+  margin-bottom:0px;
+  margin-left:16px;
+}
+
 </style>
 <section class="content-header">
     <h1>
-        Ajouter un produit 
+        Ajouter une annonce 
       <small>labshare</small>
     </h1>
     <ol class="breadcrumb">
         <li class="active"><i class="fa fa-dashboard"></i> Menu</li>
         <li><a href="#"> Mes annonces </a></li>
-        <li><a href="#"> Ajout annonce </a></li>
+        <li><a href="#"> Ajouter une annonce </a></li>
       </ol>
   </section>
   <section class="content">
@@ -75,30 +88,48 @@
               <div class="box">
                 <div class="box-body">
                 @if (Session::has('message-success-ajout-produit'))
-    <div class="alert alert-success alert-dismissible">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
-            <h4><i class="icon fa fa-check"></i> alerte !</h4>
-            {{ Session::get('message-success-ajout-produit') }}
-    </div>
-    @endif
+                        <div class="alert alert-success alert-dismissible">
+                                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">×</button>
+                                <h4><i class="icon fa fa-check"></i> alerte !</h4>
+                                {{ Session::get('message-success-ajout-produit') }}
+                        </div>
+                @endif
                   <div class="row">
-                      <form class="form-horizontal" method="post"   action="{{ route('addproduit') }}">
+                      <form class="form-horizontal form-submit-annonce" method="post"   action="">
                       @csrf
-                          <div class="box-body">
-                            <div class="form-group">
-                              <label for="reference" class="col-sm-2 control-label">Référence</label>
+                          <div class="box-body ">
+                            <div class="form-group typeannonce">
+                              <label for="typeannonce" class="col-sm-2 control-label">Type d'annonce</label>
             
                               <div class="col-sm-10">
-                                <input type="text" class="form-control" name="reference" id="reference" placeholder="Référence" required>
-                                @if ($errors->has('reference'))
+                                    <select name="typeannonce" id="typeannonce" class="form-control">
+                                            <option value="Offre">Offre</option>
+                                            <option value="Demande" >Demande</option>
+                                    </select>
+                                @if ($errors->has('typeannonce'))
                                     <span class="help-block">
-                                        <strong>{{ $errors->first('reference') }}</strong>
+                                        <strong>{{ $errors->first('typeannonce') }}</strong>
                                     </span>
                                 @endif
                               </div>
                             </div>
-                            <div class="form-group">
-                              <label for="designation" class="col-sm-2 control-label">Désignation</label>
+                            <div class="form-group natureAnnonce">
+                                    <label for="typeoffre"  class="col-sm-2 control-label">Nature d'annonce</label>
+                  
+                                    <div class="col-sm-10">
+                                          <select name="natureAnnonce" id="natureAnnonce" class="form-control">    
+                                                  <option value="Changement">Changement </option>
+                                                  <option value="Don" >Don</option>
+                                          </select>
+                                      @if ($errors->has('natureAnnonce'))
+                                          <span class="help-block">
+                                              <strong>{{ $errors->first('natureAnnonce') }}</strong>
+                                          </span>
+                                      @endif
+                                    </div>
+                            </div>
+                            <div class="form-group designation">
+                              <label for="designation" class="col-sm-2 control-label ">Désignation </label>
             
                               <div class="col-sm-10">
                                   <textarea class="form-control" name="designation" id="designation" rows="3" placeholder="Enter ..."></textarea>
@@ -109,61 +140,102 @@
                                 @endif
                               </div>
                             </div>
-                            <div class="form-group">
-                                <label for="formule" class="col-sm-2 control-label" >Formule</label>
-              
-                                <div class="col-sm-10">
-                                  <input type="text" class="form-control" name="formule" id="formule" placeholder="Formule chimique exemple: h2o" required>
-                                  @if ($errors->has('formule'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('formule') }}</strong>
-                                    </span>
-                                  @endif
-                                </div>
-                              </div>
-                              <div class="form-group">
-                                  <label for="unite" class="col-sm-2 control-label" >Unité</label>
-                
-                                  <div class="col-sm-10">
-                                    <input type="text" class="form-control" name="unite" id="unite" placeholder="Unité exemple: Litre" required>
-                                    
-                                    @if ($errors->has('unite'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('unite') }}</strong>
-                                    </span>
-                                  @endif
-                                  </div>
-                                </div>
-                                <div class="form-group">
-                                    <label for="qte" class="col-sm-2 control-label" >Quantité</label>
-                  
-                                    <div class="col-sm-10">
-                                      <input type="number" class="form-control" name="qte" id="qte" placeholder="Quantité exemple: 10" required>
-                                      @if ($errors->has('qte'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('qte') }}</strong>
-                                    </span>
-                                      @endif
-                                    </div>
-                                  </div>
-                                  <div class="form-group">
-                                      <label for="categorie" class="col-sm-2 control-label" >Catégorie</label>
-                    
-                                      <div class="col-sm-10">
-                                        <input type="text" class="form-control" name="categorie" id="categorie" placeholder="Catégorie exemple: gaz" required>
-                                        @if ($errors->has('categorie'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('categorie') }}</strong>
-                                    </span>
-                                        @endif
-                                      </div>
+                         
+                             <div class="form-group ProduitAdonner">
+                                        <label for="designation" class="col-sm-2 control-label ">Votre produit </label>
+                                 <div class="col-md-10">
+                                        <div class="row" >
+                                                <div class="col-md-4">
+                                                    <div class="form-group ">
+                                                            <div class="col-sm-12">
+                                                                      <select name="refproduit" id="refproduit" class="form-control">
+                                                                                 <option value="id p1" selected disabled>Référence  </option> 
+                                                                                 <option value="id p1">ref p1 </option>
+                                                                                 <option value="id p2" >ref p2</option>
+                                                                      </select>
+                                                                  @if ($errors->has('refproduit'))
+                                                                      <span class="help-block">
+                                                                          <strong>{{ $errors->first('refproduit') }}</strong>
+                                                                      </span>
+                                                                  @endif
+                                                            </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-8">
+                                                        <div class="form-group qte">
+                                                             <div class="col-sm-12">
+                                                                  <input type="number" class="form-control" name="qte" id="qte" placeholder="Quantité " >
+                                                                  @if ($errors->has('qte'))
+                                                                <span class="help-block">
+                                                                    <strong>{{ $errors->first('qte') }}</strong>
+                                                                </span>
+                                                                  @endif
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                             
+                                            </div>
+        
+                                 </div>
+
+                             </div>
+                             <div class="form-group ProduitSouhaité">
+                                    <label for="designation" class="col-sm-2 control-label ">Produit souhaité </label>
+                             <div class="col-md-10">
+                                    <div class="row" >
+                                            <div class="col-md-4">
+                                                <div class="form-group refproduitEchange">
+                                                        <div class="col-sm-12">
+                                                                  <select name="refproduitEchange" id="refproduitEchange" class="form-control">
+                                                                             <option value="id p1" selected disabled>Référence  </option> 
+                                                                             <option value="id p1">ref p1 </option>
+                                                                             <option value="id p2" >ref p2</option>
+                                                                  </select>
+                                                              @if ($errors->has('refproduitEchange'))
+                                                                  <span class="help-block">
+                                                                      <strong>{{ $errors->first('refproduitEchange') }}</strong>
+                                                                  </span>
+                                                              @endif
+                                                        </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-8">
+                                                    <div class="form-group qtetEchange">
+                                                         <div class="col-sm-12">
+                                                              <input type="number" class="form-control" name="qtetEchange" id="qtetEchange" placeholder="Quantité " >
+                                                              @if ($errors->has('qtetEchange'))
+                                                            <span class="help-block">
+                                                                <strong>{{ $errors->first('qtetEchange') }}</strong>
+                                                            </span>
+                                                              @endif
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+    
+                             </div>
+
+                         </div>
+                             
+
+                                    <div class="form-group file">
+                                            <label for="file" class="col-sm-2 control-label" >Image</label>
+                          
+                                            <div class="col-sm-10">
+                                              <input type="file" class="form-control" name="file" id="file" >
+                                              @if ($errors->has('file'))
+                                          <span class="help-block">
+                                              <strong>{{ $errors->first('file') }}</strong>
+                                          </span>
+                                              @endif
+                                            </div>
                                     </div>
                                   
                           </div>
                           <!-- /.box-body -->
                           <div class="box-footer">
                             <button type="reset" class="btn btn-default">Annuler</button>
-                            <button type="submit" class="btn btn-primary pull-right">Ajouter</button>
+                            <button type="submit" class="btn btn-success pull-right">Publier</button>
                           </div>
                           <!-- /.box-footer -->
                         </form>
@@ -172,6 +244,80 @@
             </div>
           </div>
     </div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+   <script>
    
+$(document).ready(function(){
+
+
+$("#typeannonce").change(function(){
+    
+    $('.ProduitAdonner').show();
+    $('.ProduitSouhaité').show();
+
+        if($("#typeannonce").val()=="Offre"){
+
+            if($("#natureAnnonce").val()=="Cangement"){
+                $('.ProduitAdonner').show();
+                $('.ProduitSouhaité').show();
+            }else  if($("#natureAnnonce").val()=="Don"){
+                $('.ProduitAdonner').show();
+                 $('.ProduitSouhaité').hide();
+
+            }
+
+        }else if($("#typeannonce").val()=="Demande"){
+           
+            if($("#natureAnnonce").val()=="Cangement"){
+                $('.ProduitAdonner').show();
+                $('.ProduitSouhaité').show();
+            }else  if($("#natureAnnonce").val()=="Don"){
+                $('.ProduitAdonner').hide();
+                 $('.ProduitSouhaité').show();
+
+            }
+
+        }
+    
+        
+    });
+
+    $("#natureAnnonce").change(function(){
+        
+        $('.ProduitAdonner').show();
+    $('.ProduitSouhaité').show();
+
+        if($("#typeannonce").val()=="Offre"){
+
+            if($("#natureAnnonce").val()=="Cangement"){
+                $('.ProduitAdonner').show();
+                $('.ProduitSouhaité').show();
+            }else  if($("#natureAnnonce").val()=="Don"){
+                $('.ProduitAdonner').show();
+                 $('.ProduitSouhaité').hide();
+
+            }
+
+        }else if($("#typeannonce").val()=="Demande"){
+           
+            if($("#natureAnnonce").val()=="Cangement"){
+                $('.ProduitAdonner').show();
+                $('.ProduitSouhaité').show();
+            }else  if($("#natureAnnonce").val()=="Don"){
+                $('.ProduitAdonner').hide();
+                 $('.ProduitSouhaité').show();
+
+            }
+
+        }
+  
+});
+
+
+});
+
+ 
+   </script>
 </section>
 @endsection
