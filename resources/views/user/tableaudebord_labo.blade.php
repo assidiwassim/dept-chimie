5e7e7beb0e7592752aa7f2ff4d9e4a75c5b29a0a
sayfoun <saifeddinhajji@gmail.com>
2019-01-24 18:32:37 +0100

    sdf

--- resources/views/user/tableaudebord_labo.blade.php
+++ resources/views/user/tableaudebord_labo.blade.php
@@ -159,7 +159,7 @@
                   <span class="progress-number"><b>{{DB::table('annonces')->whereuser_id(Auth::user()->id)->count()}}</b>/{{DB::table('annonces')->count()}}</span>
 
                   <div class="progress sm">
-                    <div class="progress-bar progress-bar-aqua" style="width:{{((DB::table('annonces')->whereuser_id(Auth::user()->id)->count())*100)/(DB::table('annonces')->count())}}%"></div>
+                    <div class="progress-bar progress-bar-aqua" style="width:{{(( DB::table('annonces')->whereuser_id(Auth::user()->id)->count())*100)/(DB::table('annonces')->count())}}%"></div>
                   </div>
                 </div>
                 <!-- /.progress-group -->
@@ -168,7 +168,7 @@
                   <span class="progress-number"><b>{{DB::table('annonces')->whereuser_id(Auth::user()->id)->wheretypeannonce("Offre")->count()}}</b>/{{DB::table('annonces')->whereuser_id(Auth::user()->id)->count()}}</span>
 
                   <div class="progress sm">
-                    <div class="progress-bar progress-bar-red" style="width: 80%"></div>
+                    <div class="progress-bar progress-bar-red" style="width: {{(( DB::table('annonces')->whereuser_id(Auth::user()->id)->wheretypeannonce('Offre')->count()) *100)/(DB::table('annonces')->whereuser_id(Auth::user()->id)->count()) }}%"></div>
                   </div>
                 </div>
                 <!-- /.progress-group -->
@@ -177,7 +177,7 @@
                   <span class="progress-number"><b>{{DB::table('annonces')->whereuser_id(Auth::user()->id)->wheretypeannonce("Demande")->count()}}</b>/ {{DB::table('annonces')->whereuser_id(Auth::user()->id)->count()}}</span>
 
                   <div class="progress sm">
-                    <div class="progress-bar progress-bar-green" style="width: 80%"></div>
+                    <div class="progress-bar progress-bar-green" style="width: {{(( DB::table('annonces')->whereuser_id(Auth::user()->id)->wheretypeannonce('Demande')->count() ) *100)/(DB::table('annonces')->whereuser_id(Auth::user()->id)->count() )}}%"></div>
                   </div>
                 </div>
                 <!-- /.progress-group -->
@@ -186,7 +186,7 @@
                   <span class="progress-number"><b>{{DB::table('produits')->whereuser_id(Auth::user()->id)->count()}}</b>/{{DB::table('produits')->count()}}</span>
 
                   <div class="progress sm">
-                    <div class="progress-bar progress-bar-yellow" style="width: 80%"></div>
+                    <div class="progress-bar progress-bar-yellow" style="width: {{ ( (DB::table('produits')->whereuser_id(Auth::user()->id)->count())*100)/(DB::table('produits')->count()) }}%"></div>
                   </div>
                 </div>
                 <!-- /.progress-group -->
@@ -196,46 +196,7 @@
             <!-- /.row -->
           </div>
           <!-- ./box-body -->
-          <div class="box-footer">
-            <div class="row">
-              <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                  <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 17%</span>
-                  <h5 class="description-header">$35,210.43</h5>
-                  <span class="description-text">TOTAL REVENUE</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                  <span class="description-percentage text-yellow"><i class="fa fa-caret-left"></i> 0%</span>
-                  <h5 class="description-header">$10,390.90</h5>
-                  <span class="description-text">TOTAL COST</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-3 col-xs-6">
-                <div class="description-block border-right">
-                  <span class="description-percentage text-green"><i class="fa fa-caret-up"></i> 20%</span>
-                  <h5 class="description-header">$24,813.53</h5>
-                  <span class="description-text">TOTAL PROFIT</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-              <!-- /.col -->
-              <div class="col-sm-3 col-xs-6">
-                <div class="description-block">
-                  <span class="description-percentage text-red"><i class="fa fa-caret-down"></i> 18%</span>
-                  <h5 class="description-header">1200</h5>
-                  <span class="description-text">GOAL COMPLETIONS</span>
-                </div>
-                <!-- /.description-block -->
-              </div>
-            </div>
-            <!-- /.row -->
-          </div>
+        
           <!-- /.box-footer -->
         </div>
         <!-- /.box -->
