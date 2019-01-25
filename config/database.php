f16b1b406ce1c68f666bc238b8c8c0a6e1ad62c0
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-01-25 20:06:01 +0100

    s

--- config/database.php
+++ config/database.php
@@ -46,7 +46,7 @@ return [
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'dept-chimie-db'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'root'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -55,7 +55,21 @@ return [
             'strict' => true,
             'engine' => null,
         ],
-
+        'mysql2' => [
+        
+            'driver' => 'mysql',
+            'host' => '167.114.153.98',
+            'port' => '3307',
+            'database' => 'dept-chimie-db',
+            'username' => 'root',
+            'password' => 'root',
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'options'=>['mode'=>'ssl'],
+            'strict' => false,
+            'engine' => null,
+        ],
         'pgsql' => [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
--- resources/views/user/Annonces_labo.blade.php
+++ resources/views/user/Annonces_labo.blade.php
@@ -230,10 +230,33 @@
                 @if( $annonce->user_id!=Auth::user()->id)
                 
                         @if($annonce->typeannonce=="Offre")
-                            <a href="#" class="btn btn-default btn-lg btn-block btn-repondre ">Répondre</a>
+                            <a href="#" class="btn btn-default btn-lg btn-block btn-repondre "  data-toggle="modal" data-target="#modal-default">Répondre</a>
                         @else
-                            <a href="#" class="btn btn-default btn-lg btn-block btn-repondre-Demande ">Répondre</a>
+                            <a href="#" class="btn btn-default btn-lg btn-block btn-repondre-Demande  " data-toggle="modal" data-target="#modal-default">Répondre</a>
                         @endif
+                  
+                    <!-- .modal-content -->
+                        <div class="modal fade" id="modal-default" style="display: none;">
+                            <div class="modal-dialog">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">×</span></button>
+                                  <h4 class="modal-title">Default Modal</h4>
+                                </div>
+                                <div class="modal-body">
+                                  <p>One fine body…</p>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Close</button>
+                                  <button type="button" class="btn btn-primary">Save changes</button>
+                                </div>
+                              </div>
+                              <!-- /.modal-content -->
+                            </div>
+                            <!-- /.modal-dialog -->
+                          </div>
+
                 @else
                 <a class="btn btn-default btn-lg btn-block " disabled>Répondre</a>
                 @endif
