36d57af4f8b0933c868fb600171a54249de90487
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-01-26 21:57:11 +0100

    vv

---
+++ resources/views/user/MesAnnonce_offre.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.layout-labo')
+
+@section('content')
+<style>
+.info-box {
+    box-shadow: 0px 3px 13px 3px #00000014;
+}
+
+.box {
+    position: relative;
+    border-radius: 3px;
+    background: #ffffff;
+    border-top: none;
+    margin-bottom: 20px;
+    width: 100%;
+    box-shadow: 0px 3px 13px 3px #00000014;
+    padding: 0;
+}
+
+
+#search-form .input-container{
+    margin: 0;
+    padding-right: 0;
+}
+#search-form input[type=text]{
+    margin: 0 !important;
+    border: none;
+    border-right: 2px solid #d2d6de;
+    font-size: 17px;
+    width: 100;
+}
+
+#search-form .select1{
+    margin: 0 !important;
+    border: none;
+    border-right: 2px solid #d2d6de;
+    color: #989a9e;
+    font-size: 17px;
+    width: 100;
+}
+#search-form .select2{
+    margin: 0 !important;
+    border: none;
+    color: #989a9e;
+    font-size: 17px;
+    width: 100;
+}
+
+#search-form .btn{
+    margin: 0 !important;
+    border-radius: 3px;
+    font-size: 19px;
+}
+.row_list_produit .box{
+    padding: 15px;
+}
+
+</style>
+<section class="content-header">
+    <h1>
+        Mes offres
+      <small>labshare</small>
+    </h1>
+    <ol class="breadcrumb">
+       
+      <li class="active"><i class="fa fa-dashboard"> </i> Menu</li>
+      <li><a href="#"> Annonce </a></li>
+      <li><a href="#">  Mes offres </a></li>
+    </ol>
+  </section>
+  <section class="content">
+ 
+     
+</section>
+@endsection
