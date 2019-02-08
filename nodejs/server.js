6c22b441bd9acb7d65dbceeeae6d44791217b982
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-02-08 20:11:48 +0100

    zed

--- nodejs/server.js
+++ nodejs/server.js
@@ -2,6 +2,12 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function(socket){
   console.log('a user connected');
   
@@ -13,11 +19,9 @@ io.on('connection', function(socket){
   });
  
 });
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
-server.listen(3000, function(){
-  console.log('listening on :3000');
+
+server.listen(3002, function(){
+  console.log('listening on :3002');
 });
 
 
