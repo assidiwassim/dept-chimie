aa676991a644fb17b466f4631841643aed6179a1
assidiwassim <29416254+assidiwassim@users.noreply.github.com>
2019-02-19 14:00:19 +0100

    chat

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,7 +21,19 @@
 
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" type="text/css" rel="stylesheet">
 <!---------------------------------------------------------------------------------------->
-  </head>
+
+<style>
+  .search-bar{
+    outline: none;
+  }
+  #m{
+    outline: none;
+  }
+  .action_on_user{
+    cursor: pointer;
+  }
+</style>
+</head>
   <body>
 
     
@@ -45,7 +57,7 @@
 
                       @foreach(DB::table('users')->where('role','<>',"admin")->where('id','<>',Auth::user()->id)->select()->get() as $user)
 
-                      <div class="chat_list active_chat action_on_user" >
+                      <div class="chat_list  action_on_user" >
                         <input type="hidden" value="{{$user->id}}" >
                         <div class="chat_people">
                           <div class="chat_img"> <img src="/upload/logo/{{$user->logo}}" alt="sunil"> </div>
@@ -79,66 +91,6 @@
                     </div>
                   </div>
                 </div>
-                
-               
-         
-            
-            
-         
-    <!-------------------------------------------------------------------------
-<ul id="messages">
-  </ul>
-  
-    <form action="">
-      <input id="m" autocomplete="off" /><button>Send</button>
-    </form>
-    
-<script>
-
-$.ajaxSetup({
-      headers: {
-          'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-        }
-      });
-
-$(function () {
-  
-  var socket = io.connect('https://dept-chimie-chat.ml/');
-
-    socket.on('chat', function(data_msg){
-      console.log(data_msg);
-      $('#messages').append($('<li>').text(data_msg.text));
-    });
-
-    $('form').submit(function(e){
-      e.preventDefault();
-      var msg =$('#m').val();
-     
-      var data_msg = {'_token': '{{csrf_token()}}',"from":22, "to":33,"text":msg}; 
-      socket.emit('chat', data_msg);
-      $('#m').val('');
-      $.ajax({
-        type: 'POST',
-        url: 'AddMsg',
-        data: data_msg,
-        cache:false,
-        success: function (data) {
-            console.log(data);
-        },
-        error: function (data, textStatus, errorThrown) {
-            console.log(data);
-        },
-      })
-
-      return false;
-    });
-
-   
-
-  });
-
-</script>
-------------------->
 <script>
 
 $.ajaxSetup({
@@ -150,21 +102,24 @@ $.ajaxSetup({
 $(function () {
 
 
-  
+ 
 var from ;
 var to;
 
+
 $( ".action_on_user" ).click(function() {
- 
+
+
+      $(".action_on_user").each(function() {
+        $(".action_on_user").removeClass("active_chat");
+      })
+      $(this).addClass("active_chat") ;
+
+
     from = {{Auth::user()->id}} ;
     to = parseInt($(this).find( "input" ).val(), 10);
 
     var data_msg = {'_token': '{{csrf_token()}}',"from":from, "to":to}; 
-        
-                     
-                      
-                   
-
           $.ajax({
             type: 'POST',
             url: 'GetListMessage',
@@ -173,21 +128,17 @@ $( ".action_on_user" ).click(function() {
             success: function (data) {
 
               $('#messages div').remove();
+
               data.forEach(function(element) {
-                
                 if(element.from=={{Auth::user()->id}}){
-                  console.log('mab3outh');
                   $('#messages').append('<div class="outgoing_msg"><div class="sent_msg"><p>'+element.text+'</p><span class="time_date"> 11:01 AM    |    Today</span> </div></div>');
                 }
                 else{
-                 
-                  console.log("ena lbe3eth");
                   $('#messages').append('<div class="incoming_msg"><div class="incoming_msg_img"> <img src="https://ptetutorials.com/images/user-profile.png" alt="sunil"> </div><div class="received_msg"><div class="received_withd_msg"><p>'+element.text+'</p><span class="time_date"> 11:01 </span></div></div></div>');
               
                 }
                
               });
-                console.log(data);
             },
             error: function (data, textStatus, errorThrown) {
                // console.log(data);
@@ -221,26 +172,29 @@ socket.on('chat', function(data_msg){
 
 $('#form-chat').submit(function(e){
   e.preventDefault();
-  var msg =$('#m').val();
- //console.log(from,to,msg)
+  if(to!=undefined && from != undefined){
+    
+      var msg =$('#m').val();
 
- var data_msg = {'_token': '{{csrf_token()}}',"from":from, "to":to,"text":msg}; 
-      socket.emit('chat', data_msg);
-      $('#m').val('');
-      $.ajax({
-        type: 'POST',
-        url: 'AddMsg',
-        data: data_msg,
-        cache:false,
-        success: function (data) {
-          
-           // console.log(data);
-        },
-        error: function (data, textStatus, errorThrown) {
-           // console.log(data);
-        },
-      })
-      return false;
+      var data_msg = {'_token': '{{csrf_token()}}',"from":from, "to":to,"text":msg}; 
+            socket.emit('chat', data_msg);
+            $('#m').val('');
+            $.ajax({
+              type: 'POST',
+              url: 'AddMsg',
+              data: data_msg,
+              cache:false,
+              success: function (data) {
+                
+                // console.log(data);
+              },
+              error: function (data, textStatus, errorThrown) {
+                // console.log(data);
+              },
+            })
+            return false;
+        }
+
 });
 
 $( "#target" ).keyup(function() {
@@ -258,6 +212,10 @@ $( ".chat_list" ).each(function( index ) {
 
   })
 });
+
+
+$(".action_on_user").first().addClass("active_chat") ;
+$('.action_on_user').first().click();
 
 })
 </script>
