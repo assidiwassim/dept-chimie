73bc41c15e2fbab4a304798104ef1f279c44617e
sayfoun <saifeddinhajji@gmail.com>
2019-03-07 12:16:02 +0100

    dùsmjgjds

--- app/Http/Controllers/ParametreProfil.php
+++ app/Http/Controllers/ParametreProfil.php
@@ -25,8 +25,10 @@ class ParametreProfil extends Controller
       
     
        $x= User::whereemail($request->input('email'))->count();
-     if($x==0 || $x==1)
-        {$user=User::whereid(Auth::user()->id)->update(['name' => $request->input('username'),'email' => $request->input('email')]);
+       $x1= User::whereemail($request->input('email'))->where('id',Auth::user()->id)->count();
+     if(($x==0 || $x1==1))
+        {
+          $user=User::whereid(Auth::user()->id)->update(['name' => $request->input('username'),'email' => $request->input('email')]);
         session()->flash('message-success-modification-profil','Votre profil à eté  modifier avec succées');
        }
        return back()->withInput();
